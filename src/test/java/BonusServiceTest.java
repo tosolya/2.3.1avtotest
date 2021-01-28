18569721564dd9d8c46fdef201f03ae0418cab23
Olga Tarasova <bos.olga18@gmail.com>
2021-01-28 14:50:08 +0300

    created 5 files

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,66 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void calculate() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount =1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        //вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount =1_000_000_60;
+        boolean registered = true;
+        long expected = 500;
+
+        //вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateUnRegisteredAndBelowLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount =1000_60;
+        boolean registered = false;
+        long expected = 30;
+
+        //вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateUnRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount =1_000_000_60;
+        boolean registered = false;
+        long expected = 500;
+
+        //вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+}
+
+
+
